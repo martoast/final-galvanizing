0e0595307dec9935340c0e26984094eaa922aece
dev <alexmartos96@gmail.com>
2020-05-13 19:29:55 -0700

    added video to dialog

--- components/VideoDialog.vue
+++ components/VideoDialog.vue
@@ -34,7 +34,7 @@
         <div align="center">
           <iframe
             style="position:fixed; top:55px; left:0; bottom:0; right:0; width:100%; height:100vh; border:none; margin:0; padding:0; z-index:999999;"
-            src="https://bbemaildelivery.com/bbext/?p=vidEmbed&id=2a58e909-da7f-4fe3-aeb2-af8f9144ac8a"
+            src="https://drive.google.com/file/d/1TBQI9-wD-2vLzgVtwWUpG854hN-e9L2A/preview"
             frameborder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
